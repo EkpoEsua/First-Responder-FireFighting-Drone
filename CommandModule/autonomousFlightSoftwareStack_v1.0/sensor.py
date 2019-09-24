5b8c840a21c869b9caa4983a54723e7d9b0847a9
EkpoEsua <esuaekpo@yahoo.com>
2019-09-24 15:35:17 +0100

    adding new class location

--- CommandModule/autonomousFlightSoftwareStack_v1.0/sensor.py
+++ CommandModule/autonomousFlightSoftwareStack_v1.0/sensor.py
@@ -4,7 +4,22 @@ Created on Mon September 23 11:14:45 2019
 
 @author: EsuaEkpo
 
+@decription: This class encapsulates the properties of a sensor module as an object
+
 """
 
+from utilities.location import Location
 
-class Sensor(Object):
+class Sensor(object):
+
+    sensorID = 0
+
+    def __init__(self, latitude, longitude, height=0):
+        self._latitude = latitude
+        self._longitude = longitude
+        self._status = 0
+        Sensor.sensorID += 1
+        self._sensorId = Sensor.sensorID
+        
+    def getLocation(self):
+        return 
